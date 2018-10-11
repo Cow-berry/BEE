c23c856a99559fcc48a523a43eaf00aa4892d700
cowberry <tolya.cow@gmail.com>
2018-10-11 13:23:39 +0300

    painting stuff

--- app/constants.py
+++ app/constants.py
@@ -4,8 +4,5 @@ sector_size = 3
 
 # кол-во слоёв
 layers_count = 4
-# кол-во нейроннов в скрытых слоях
-hidden_layers = 4
-
-# параметры выходного слоя
-exit_layer = 3
+# кол-во нейроннов в слоях
+neuron_layers = [2*sector_size, 4, 4, 2]
--- app/window.py
+++ app/window.py
@@ -1,9 +1,11 @@
 import sys
 from PyQt5.Qt import QColor
 from PyQt5.QtWidgets import QApplication, QWidget, QMainWindow, QAction, qApp
+from PyQt5 import QtGui
 from PyQt5.QtGui import QPainter, QBrush, QPen
 from PyQt5.QtCore import Qt, QTimer
-import random
+from PyQt5.QtWidgets import QGraphicsItem, QGraphicsEllipseItem, QGraphicsLineItem, QStyleOptionGraphicsItem
+import random as rn
 import numpy as np
 
 import organism
@@ -27,7 +29,6 @@ class Main(QMainWindow):
         self.res_y = 300
         self.resource = Resource(self.res_x, self.res_y, 20)
         self.All_Cells = []
-        self.All_Visible_Cells = []
 
         self.evolution()
 
@@ -38,47 +39,49 @@ class Main(QMainWindow):
         # инициализация первого поколения
         start_distance = 200
         amount = 6
-        self.All_Visible_Cells = [Cell(np.cos(2*np.pi*i/amount)*start_distance+self.res_x, np.sin(2*np.pi*i/amount)*start_distance+self.res_y, 10) for i in range(amount)]
+        self.All_Cells = [Cell(np.cos(2*np.pi*i/amount)*start_distance+self.res_x, np.sin(2*np.pi*i/amount)*start_distance+self.res_y, 10) for i in range(amount)]
         self.change()
 
     def paintEvent(self, event):
         qp = QPainter(self)
         qp.setRenderHint(QPainter.Antialiasing, True)
         qp.setPen(QPen(Qt.black, 2, Qt.SolidLine))
-        # self.drawCell(qp, circle)
+        # self.drawResource(qp)
         self.drawResource(qp)
-        for cell in self.All_Visible_Cells:
-            self.drawCell(qp, cell)
+        for cell in self.All_Cells:
+            # self.drawCell(qp, cell)
+            pass
 
-    def drawCell(self, qp, cell):
-        qp.setBrush(QBrush(cell.color, Qt.SolidPattern))
-        qp.drawEllipse(*cell.getEllipseCoords())
+    # def drawCell(self, qp, cell):
+    #     qp.setBrush(QBrush(cell.color, Qt.SolidPattern))
+    #     qp.drawEllipse(*cell.getEllipseCoords())
+
+    # def drawResource(self, qp):
+    #     qp.setBrush(QBrush(self.resource.color, Qt.SolidPattern))
+    #     qp.drawEllipse(*self.resource.getResourceCoords())
 
     def drawResource(self, qp):
-        qp.setBrush(QBrush(self.resource.color, Qt.SolidPattern))
-        qp.drawEllipse(*self.resource.getResourceCoords())
+        qp.setBrush(QBrush(Qt.green, Qt.SolidPattern))
+        qp.setPen(QPen(Qt.black, 2, Qt.SolidLine))
+        self.resource.paint(qp, QStyleOptionGraphicsItem())
 
     def change(self):
         self.update()
         self.show()
 
-class Cell():
+class Cell(QGraphicsEllipseItem):
     def __init__(self, x, y, r, w = None, b = None):
+        super().__init__(x, y, r, r)
         if w is None:
+            w = []
             for i in range(layers_count-1):
-                self.w.append(np.array([random.uniform(-1,1) for j in range()]))
-
-#TODO обощить код для случая произвольного кол-ва слоёв
-        else:
-            self.w = w
+                w.append(np.array([[rn.uniform(-1,1) for i in range(neuron_layers[i])] for i in range(neuron_layers[i+1])]))
+        self.w = w
         if b is None:
-            # веса смещения первого и второго слоев
-            b1 = np.array([random.uniform(-1, 1) for i in range(4)])
-            b2 = np.array([random.uniform(-1, 1) for i in range(4)])
-            b3 = np.array([random.uniform(-1, 1) for i in range(2)])
-            self.b = [b1, b2, b3]
-        else:
-            self.b = b
+            b = []
+            for i in range(layers_count-1):
+                b.append(np.array([rn.uniform(-1,1) for i in range(neuron_layers[i])]))
+        self.b = b
         self.x = x
         self.y = y
         self.r = r
@@ -89,18 +92,15 @@ class Cell():
     def getEllipseCoords(self):
         return [self.x - self.r / 2, self.y - self.r / 2, self.r, self.r]
     def get_step(self, basic_data): # basic_data -- [x, y, orient, ... (health, stamina) ]
-        for i in range(self.raze_amount):
-
-
-
-        return organism.calc(*enter_data, self.w, self.b)
+        return None
     def move(x, y):
         self.x += x
         self.y += y
 
 
-class Resource():
+class Resource(QGraphicsEllipseItem):
     def __init__(self, x, y, r=10):
+        super().__init__(x, y, r, r)
         self.x = x
         self.y = y
         self.r = r
